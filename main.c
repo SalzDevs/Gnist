15d511845d93d04c13a3ee12aacb4ad2df084b98
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-20 14:34:18 +0100

    create the particle structure

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+
+typedef struct {
+  bool alive;
+  int health;
+  int x;
+  int y;
+  int radius;
+} particle;
 
 int main(void) {
-    printf("hello gnist\n");
+    particle part;
+    part.alive = true;
+    part.health = 100;
+    part.x = 0;
+    part.y = 0;
+    part.radius = 1;
+    printf("particle (alive:%d | health:%d | x:%d | y:%d | radius:%d)\n", part.alive,part.health,part.x,part.y,part.radius);
     return 0;
 }
