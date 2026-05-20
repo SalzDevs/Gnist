e42291815f5751403259b9d5b93b0ed252ec412d
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-20 14:50:31 +0100

    seperate the main struct into smaller ones

--- main.c
+++ main.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+typedef struct{
+  int x;
+  int y;
+} ParticleCenter;
+
+typedef struct{
+  ParticleCenter center; 
+  int radius;
+} ParticleCircle;
 
 typedef struct {
   bool alive;
   int health;
-  int x;
-  int y;
-  int radius;
+  ParticleCircle particleCircle;
 } Particle;
 
 void initParticle(Particle* part){
     part->alive = true;
     part->health = 100;
-    part->x = 0;
-    part->y = 0;
-    part->radius = 1;
+    part->particleCircle.radius = 10;
+    part->particleCircle.center.x = 0;
+    part->particleCircle.center.y = 0;
 }
 
 int main(void) {
     Particle part;
     initParticle(&part);
-    printf("particle (alive:%d | health:%d | x:%d | y:%d | radius:%d)\n", part.alive,part.health,part.x,part.y,part.radius);
+    printf("particle (alive:%d | health:%d | x:%d | y:%d | radius:%d)\n", part.alive,part.health,part.particleCircle.center.x,part.particleCircle.center.y,part.particleCircle.radius);
     return 0;
 }
