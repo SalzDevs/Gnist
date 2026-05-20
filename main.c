48fa66362cd75590ebb4ad7de447defdbe433ca8
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-20 14:37:10 +0100

    init particle function

--- main.c
+++ main.c
@@ -8,15 +8,19 @@ typedef struct {
   int x;
   int y;
   int radius;
-} particle;
+} Particle;
+
+void initParticle(Particle* part){
+    part->alive = true;
+    part->health = 100;
+    part->x = 0;
+    part->y = 0;
+    part->radius = 1;
+}
 
 int main(void) {
-    particle part;
-    part.alive = true;
-    part.health = 100;
-    part.x = 0;
-    part.y = 0;
-    part.radius = 1;
+    Particle part;
+    initParticle(&part);
     printf("particle (alive:%d | health:%d | x:%d | y:%d | radius:%d)\n", part.alive,part.health,part.x,part.y,part.radius);
     return 0;
 }
