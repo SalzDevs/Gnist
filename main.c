47ee36a26d8557ff7f74e981586a29930a46a6ab
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-21 14:04:01 +0100

    function to move a particle

--- main.c
+++ main.c
@@ -13,35 +13,40 @@ typedef struct {
 } Particle;
 
 void initParticle(Particle* part, float x, float y){
-    part->alive = true;
-    part->health = 100;
-    part->radius = 10;
-    part->center = (Vector2) {x,y}; 
+  part->alive = true;
+  part->health = 100;
+  part->radius = 10;
+  part->center = (Vector2) {x,y}; 
+}
+
+void moveParticle(Vector2 moveVec,Particle *part){
+  part->center.x += moveVec.x;
+  part->center.y += moveVec.y;
 }
 
 int main(void) {
-    Particle particles[1];
-    
-    initParticle(&particles[0],WINDOW_WIDTH/2,WINDOW_HEIGHT/2);
-    
-    size_t length_particles = sizeof(particles)/sizeof(particles[0]);
-    
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Gnist");
+  Particle particles[1];
 
-    SetTargetFPS(60);              
+  initParticle(&particles[0],WINDOW_WIDTH/2,WINDOW_HEIGHT/2);
+  moveParticle((Vector2){100,-50}, &particles[0]);
+  size_t length_particles = sizeof(particles)/sizeof(particles[0]);
 
-    
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Gnist");
 
-        for (int i = 0; i<length_particles; i++) {
-          DrawCircle(particles[i].center.x, particles[i].center.y, particles[i].radius, MAROON); 
-        }
+  SetTargetFPS(60);              
 
-        EndDrawing();
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    for (int i = 0; i<length_particles; i++) {
+      DrawCircle(particles[i].center.x, particles[i].center.y, particles[i].radius, MAROON); 
     }
 
-    CloseWindow();
-    return 0;
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
