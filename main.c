5b962fbde6b2dbd7c838a2c69f6bbcd81cf13957
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-21 22:14:50 +0100

    add random movement to particles

--- main.c
+++ main.c
@@ -33,33 +33,36 @@ void initParticles(size_t size, Particles *particles){
   particles->arr = malloc(size*sizeof(Particle));
 }
 
-void moveParticle(Vector2 moveVec,Particle *part){
-  part->center.x += moveVec.x;
-  part->center.y += moveVec.y;
-}
-
 void addParticle(Particles *particles, Particle part){
   if (particles->count+1 > particles->size){
     particles->size+=20;
-    particles->arr = realloc(particles->arr, particles->size * sizeof(Particles));
+    particles->arr = realloc(particles->arr, particles->size * sizeof(Particle));
   }
   particles->arr[particles->count] = part;
   particles->count++;
 }
 
+void moveParticle(Particle *part){
+    Vector2 move = {GetRandomValue(-10,10),GetRandomValue(-10,10)};
+    part->center.x += move.x;
+    part->center.y += move.y;
+}
+
 int main(void) {
   Particles particles;
   initParticles(50,&particles);
-
-  /*spanw the first particle*/ 
+  float rand_x = (float)GetRandomValue(0, WINDOW_WIDTH);
+  int rand_y = (float)GetRandomValue(0, WINDOW_HEIGHT);
+  /* spanw the first particle */ 
   Particle part;
-  initParticle(&part, 200, 100);
+  initParticle(&part, rand_x, rand_y);
   addParticle(&particles, part);
 
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Gnist");
-
-  SetTargetFPS(60);              
+  
   float spawnTimer = 0.0f;
+  SetTargetFPS(60);      
+
   while (!WindowShouldClose()) {
     float dt = GetFrameTime();
 
@@ -68,14 +71,18 @@ int main(void) {
 
     spawnTimer+=dt;
     if (spawnTimer > 5.0f){
+      rand_x = (float)GetRandomValue(0, WINDOW_WIDTH);
+      rand_y = (float)GetRandomValue(0, WINDOW_HEIGHT);
+
       Particle part;
-      initParticle(&part, 200, 100);
+      initParticle(&part, rand_x, rand_y);
       addParticle(&particles, part);
       spawnTimer -= 5.0f;
     }
 
     for (size_t i = 0; i<particles.count; i++) {
-      DrawCircle(particles.arr[i].center.x, particles.arr[i].center.y, particles.arr[i].radius, MAROON); 
+      DrawCircle(particles.arr[i].center.x, particles.arr[i].center.y, particles.arr[i].radius, MAROON);
+      moveParticle(&particles.arr[i]);
     }
 
     EndDrawing();
