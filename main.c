1eb0109088606c042dd5e0f38f3ee44866154f7a
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-21 14:29:33 +0100

    function to initiate particles and to add particle to particles

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "raylib.h"
 
 #define WINDOW_WIDTH 800 
@@ -12,6 +13,12 @@ typedef struct {
   int radius;
 } Particle;
 
+typedef struct{
+  size_t size;
+  size_t count;
+  Particle** arr;
+} Particles;
+
 void initParticle(Particle* part, float x, float y){
   part->alive = true;
   part->health = 100;
@@ -19,16 +26,42 @@ void initParticle(Particle* part, float x, float y){
   part->center = (Vector2) {x,y}; 
 }
 
+
+void initParticles(size_t size, Particles *particles){
+  particles->size = size;
+  particles->count = 0;
+  particles->arr = malloc(size*sizeof(Particle));
+}
+
 void moveParticle(Vector2 moveVec,Particle *part){
   part->center.x += moveVec.x;
   part->center.y += moveVec.y;
 }
 
-int main(void) {
-  Particle particles[1];
+void addParticle(Particles *particles, Particle *part){
+  if (particles->count+1 > particles->size) particles->size+=20;
+  particles->arr[particles->count] = part;
+  particles->count++;
+}
 
-  initParticle(&particles[0],WINDOW_WIDTH/2,WINDOW_HEIGHT/2);
-  moveParticle((Vector2){100,-50}, &particles[0]);
+int main(void) {
+  Particles particles;
+  Particle part;
+  initParticle(&part,100,200);
+  initParticles(3,&particles);
+  printf("Particles: (count:%zu size:%zu)\n",particles.count,particles.size);
+  addParticle(&particles, &part);
+  addParticle(&particles, &part);
+  addParticle(&particles, &part);
+  printf("Particles: (count:%zu size:%zu)\n",particles.count,particles.size); 
+  addParticle(&particles, &part);
+  addParticle(&particles, &part);
+  printf("Particles: (count:%zu size:%zu)\n",particles.count,particles.size);
+  for (int i=0;i<particles.count;i++){
+    printf("Particle number {%d} (alive:%d health:%d radius:%d x:%f y:%f)\n",i,particles.arr[i]->alive,particles.arr[i]->health,particles.arr[i]->radius,particles.arr[i]->center.x,particles.arr[i]->center.y);
+  }
+  //moveParticle((Vector2){100,-50}, &particles[0]);
+  /*
   size_t length_particles = sizeof(particles)/sizeof(particles[0]);
 
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Gnist");
@@ -48,5 +81,6 @@ int main(void) {
   }
 
   CloseWindow();
+  */
   return 0;
 }
